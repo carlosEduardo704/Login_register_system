48e433120ce0ea7b09642688583b099383ac96fa
Carlos <carlos704eduardo@gamil.com>
2026-08-10 18:07:57 -0300

    fix: bug not create otp if user is already in database

--- authentication/services/signup.py
+++ authentication/services/signup.py
@@ -1,18 +1,30 @@
 from authentication.models import User, OtpToken
+from .emails.send_email_otp_token_service import send_email_otp
+
 
 def start_signup(email):
     user, created = User.objects.get_or_create(
         email=email,
         defaults = {
-            "is_actIve": False,
+            "is_active": False,
             "email_verified": False
         }
     )
+
+    user.set_unusable_password()
+    user.save()
+
+    OtpToken.objects.filter(
+        user=user, 
+        purpose=OtpToken.OtpPurpose.AUTHENTICATION, 
+        used=False
+    ).update(used=True)
+
     
-    if created:
-        OtpToken.generate_new_otp_token(user, purpose="AUTHENTICATION")
-        
-        # send otp token email
+    purpose = OtpToken.OtpPurpose.AUTHENTICATION
+    otp_token = OtpToken.generate_new_otp_token(user, purpose)
     
+    # send_email_otp(user.email, otp_token)
+
 
     return user
