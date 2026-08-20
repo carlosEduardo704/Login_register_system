e0313b92f6f3d9b75a62a140a2ad6ed3b7476b3d
Carlos <carlos704eduardo@gamil.com>
2026-08-20 18:34:09 -0300

    organizing code

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path, reverse_lazy
-from authentication.views import HomePageView, SignupView, CustomLoginView, ForgetPasswordView
+from django.urls import path
+from authentication.views import (
+    HomePageView,
+    SignupView,
+    CustomLoginView,
+    ForgetPasswordView,
+    CustomLogoutView,
+    PasswordResetConfirmView
+)
 from django.contrib.auth.views import LogoutView, PasswordResetConfirmView
 from authentication.forms import CustomSetPasswordForm
 
@@ -7,16 +14,8 @@ urlpatterns = [
     path("", HomePageView.as_view(), name="home_page"),
     path("signup/", SignupView.as_view(), name="signup"),
     path("login/", CustomLoginView.as_view(), name="login"),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', CustomLogoutView.as_view(next_page='login'), name='logout'),
     path("forget_password/", ForgetPasswordView.as_view(), name="forget_password"),
-    path(
-        "reset_password/<uidb64>/<token>/",
-        PasswordResetConfirmView.as_view(
-            success_url=reverse_lazy("login"),
-            template_name="password_reset_confirmation.html",
-            form_class=CustomSetPasswordForm
-        ),
-        name="reset_password"
-    )
+    path("reset_password/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name="reset_password")
     
 ]
