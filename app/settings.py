3a8b98c5432d6404036cc810902a527f0defaa89
Carlos <carlos704eduardo@gamil.com>
2026-08-26 21:36:29 -0300

    deploy

--- app/settings.py
+++ app/settings.py
@@ -157,3 +157,6 @@ CACHES = {
 CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
+
+# STATIC ROOT config
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
